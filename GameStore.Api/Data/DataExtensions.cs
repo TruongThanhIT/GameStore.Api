facc149ae32f4c1479102ef4153afd68b6996038
Truong Huynh Thuy Thai Thanh <thanh.truonghuynhthuythai@hotmail.com>
2026-03-31 21:00:27 +1300

    Implemented DI, async, await for handle API Call and database

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -14,7 +14,8 @@ public static class DataExtensions
 
     public static void AddGameStoreDb(this WebApplicationBuilder builder)
     {
-        var connString = builder.Configuration.GetConnectionString("GameStore");;
+        var connString = builder.Configuration.GetConnectionString("GameStore");
+        builder.Services.AddScoped<GameStoreContext>();
         builder.Services.AddSqlite<GameStoreContext>(
             connString,
             optionsAction: options => options.UseSeeding((context, _) =>
